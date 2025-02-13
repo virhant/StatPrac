e4ef6fffdb71471ea54a63e20dfde32e01f77f98
virhant <dimgalp@gmail.com>
2025-02-13 21:36:41 +0100

    LEGO

--- 1task/1.1.py
+++ 1task/1.1.py
@@ -1,2 +1,2 @@
-t = ([x for x in range(12345) if x ** 2 % 3 == 0 and x ** 2 % 4 == 0 and x ** 2 % 8 != 0])
+t = ([x for x in range(112) if x ** 2 % 3 == 0 and x ** 2 % 4 == 0 and x ** 2 % 8 != 0 and x ** 2 < 12345])
 print(t)
