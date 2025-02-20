0bc79d0e4bc1e912c57bcff9807bcf4d1916e8e6
virhant <dimgalp@gmail.com>
2025-02-20 19:31:32 +0100

    naf

---
+++ 2task/2.1.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def beauty_derivative_plot(px):
+    f1 = lambda x: 2 * np.sin(x) - np.cos(2 * x)
+    f2 = lambda x: 2 * np.cos(x) + 2 * np.sin(2 * x)
+
+    py = f1(px)
+
+    x = np.linspace(-2 * np.pi, 2 * np.pi, 100)
+    x[0] = -2 * np.pi
+    x[-1] = 2 * np.pi
+    y1 = f1(x)
+    y2 = f2(x)
+
+    plt.figure(figsize=(10, 5))
+
+    plt.plot(x, y1, "b-", label = "f(x) = 2sin(x) - cos(2x)")
+    plt.plot(x, y2, "r--", label = "f'(x) = 2cos(x) + 2sin(2x)")
+    plt.plot(px, py, "ko", zorder = 3)
+
+    plt.plot([px - 1.2, px + 1.2], [f2(px) * (-1.2) + py, f2(px) * 1.2 + py], "g-", label = f"Tangent at x = {px}")
+    plt.plot([px, px, 0], [0, py, py], color = "purple", linestyle = "--", alpha = 0.5)
+    plt.annotate(f"f({px}) = {py:.1f}", xy = (px, py), xytext = (px + 0.8, py - 2), color = "black", arrowprops = {"arrowstyle" : "->", "connectionstyle" : "arc3, rad = 0.2"})
+
+    plt.legend(loc = "best")
+    plt.axhline(0, color = 'black', linewidth = 1)
+    plt.axvline(0, color = 'black', linewidth = 1)
+    plt.xlim(-2 * np.pi, 2 * np.pi)
+    
+    plt.title("Function, derivative and tangent line")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    
+    plt.grid(True)
+
+    plt.show()
+    
+for x in [-3, -2, 1.7]:
+    beauty_derivative_plot(x)
--- tedt.py
+++ tedt.py
@@ -1,24 +1,32 @@
-str = "hello world"
-print(str)
-print(str, len(str))
+import matplotlib.pyplot as plt
+import numpy as np
 
-str1 = "pin" + ' ' + "code"
-print(str1)
+f1 = lambda x: 2 * np.sin(x) - np.cos(2 * x)
+f2 = lambda x: 2 * np.cos(x) + 2 * np.sin(2 * x)
 
-arr = [1, 2, "lol"]
-print(arr, arr[-1])
-arr1 = arr.pop()
-print(arr1, arr)
+px = -2.4
+py = f1(px)
 
-dict = {"cat" : "milka", "dog" : "chelsea"}
-print(dict, len(dict))
-print(dict["cat"])
+x = np.linspace(-2 * np.pi, 2 * np.pi, 100)
+x[0] = -2 * np.pi
+x[-1] = 2 * np.pi
+y1 = f1(x)
+y2 = f2(x)
 
-t = (1, 1, 3, 4)
-print(t, len(t))
-t1 = (1)
-t2 = (1,)
-print(type(t1), type(t2))
+plt.figure(figsize=(10, 5))
 
-mas = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-print(mas[0][1])
+plt.plot(x, y1, "b-", label = "f(x) = 2sin(x) - cos(2x)")
+plt.plot(x, y2, "r--", label = "f'(x) = 2cos(x) + 2sin(2x)")
+plt.plot(px, py, "ko", zorder = 3)
+
+plt.plot([px - 1.2, px + 1.2], [f2(px) * (-1.2) + py, f2(px) * 1.2 + py], "g-", label = f"Tangent at x = {px}")
+plt.plot([px, px, 0], [0, py, py], color = "purple", linestyle = "--", alpha = 0.5)
+plt.annotate(f"f({px}) = {py:.1f}", xy = (px, py), xytext = (px + 0.8, py - 2), color = "black", arrowprops = {"arrowstyle" : "->", "connectionstyle" : "arc3, rad = 0.2"})
+
+plt.legend(loc = "best")
+plt.axhline(0, color = 'black', linewidth = 1)
+plt.axvline(0, color = 'black', linewidth = 1)
+plt.xlim(-2 * np.pi, 2 * np.pi)
+plt.grid(True)
+
+plt.show()
